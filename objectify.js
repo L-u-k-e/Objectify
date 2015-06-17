18fd15640a81dcfed511bf60506a66f92e96516f
Lucas Parzych <parzycl1@sunyit.edu>
2015-06-17 16:29:31 -0400

    minify and document source
    add minified version
    and add in-source documentation

--- objectify.js
+++ objectify.js
@@ -1,3 +1,42 @@
+/**************************************************************************
+  Objectify.js  version: 1.0.1                                          
+                                                                        
+  Author: Lucas Parzych                                                 
+  MIT License: You are free to use this code however you want. Have Fun!
+**************************************************************************/
+
+
+
+/*
+Description:
+  This is the function you call to register your object. 
+
+Parameters:
+  instance     {object}: The object you need to reference from your
+                         DOM elements.
+  element_type {string}: Tag name to be passed to document.createElement()
+  properties   {object}: A hash that holds property names/values you want to
+                         assign to the containing element. 
+
+Behaviour:
+  An element of type $element_type will be created with the properties 
+  specified in $properties. It will also have an 'object_' property, which
+  points to $instance. A property called 'element' will be added to $instance,
+  which points to the newly created element.
+
+Hints:
+  The argument passed as $instance should usually be 'this' and the function 
+  should usually be called at the top of a class definition.
+
+Example:
+  function MyClass(){
+    register(this, 'div', {className: 'my-class'});
+    
+    console.log(this.element);           // [HTMLDivElement]
+    console.log(this.element.object_);   // (this)
+    console.log(this.element.className); // 'my-class' 
+  }   
+*/   
 function register(instance, element_type, properties){  
   instance.element = document.createElement(element_type);
   instance.element.object_ = instance;
@@ -6,19 +45,16 @@ function register(instance, element_type, properties){
   }
 }
 
-//function to recursively tether an element and all of its children to 
-//an object. This function will NOT overwrite predefined object trees. 
-function __tether(instance, element){
-  if(element.object_){ instance = element.object_; }
-  element.object_ = instance;
-  //console.log(element + ' has been tethered to: ' + instance);
-  for(var i=0; i<element.children.length; i++){
-    __tether(instance, element.children[i]);
-  }
-}
 
-//observe document for addition of new nodes and assign them the proper
-//object_ property if applicable.
+
+
+
+
+
+
+
+// Observe document for the addition of new nodes.
+// Assign them the proper object_ property if applicable.
 var __observer = new MutationObserver(function(mutations){
   mutations.forEach(function(mutation){
     var new_nodes = mutation.addedNodes;
@@ -34,3 +70,16 @@ var __observer = new MutationObserver(function(mutations){
 });
 var __config = { subtree: true, childList: true };
 __observer.observe(document, __config);
+
+
+
+// Recursively tether an element and all of its children to object. 
+// This function will NOT overwrite predefined object trees. 
+function __tether(instance, element){
+  if(element.object_){ instance = element.object_; }
+  element.object_ = instance;
+  //console.log(element + ' has been tethered to: ' + instance);
+  for(var i=0; i<element.children.length; i++){
+    __tether(instance, element.children[i]);
+  }
+}
